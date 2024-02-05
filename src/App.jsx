faa50272236f38c31f3b18744d1607cc127dd6a1
Saurabh23089 <saurabha9540@gmail.com>
2024-02-05 15:17:17 +0530

    changed bg color

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-     <div className=''>
+     <div className='bg-[#ffffff]'>
       <Toppart/>
       <Intro/>
       <About/>
